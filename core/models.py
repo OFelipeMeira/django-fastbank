59a8f98ffb06295a1bc7da765362a75d617e0055
OFelipeMeira <felipecmeira2004@gmail.com>
2023-11-17 13:51:37 -0300

    aula 4 - user model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,46 @@
-from django.db import models
 
-# Create your models here.
+
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import( 
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("User must have an email address")
+    
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save()
+
+        return user
+    
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_supersuser = True
+        user.save(using=self._db)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=255, null=False)
+    last_name = models.CharField(max_length=255, null=False)
+    cpf = models.CharField(max_length=255, null=False)
+    # url_imagem = models.ImageField(null=True, upload_to=user_image_field)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created = models.DateTimeField(default=timezone.now)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
